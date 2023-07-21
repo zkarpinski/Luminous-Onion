291aeffe0bdcb4c55812b8f3ecca0513cee83c38
Zachary Karpinski <zkarpi1@wgu.edu>
2023-07-21 14:13:42 -0400

    init project with spring-boot.

---
+++ src/main/java/com/zacharykarpinski/luminousonion/LuminousOnionApplication.java
@@ -0,0 +1,13 @@
+package com.zacharykarpinski.luminousonion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminousOnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminousOnionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zacharykarpinski/luminousonion/LuminousOnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharykarpinski.luminousonion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminousOnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
