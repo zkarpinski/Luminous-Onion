9c9b937263b44e2303db653cd9bcf0c075ec857b
Zachary Karpinski <zkarpi1@wgu.edu>
2023-09-26 21:32:04 -0400

    revert change

--- core/src/main/UI/src/pages/Login.js
+++ core/src/main/UI/src/pages/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const [state, setState] = useState({ username: "", password: "" });
 
   useEffect(() => {
-    setState({ username: "", password: ""});
+    setState({ username: "karpinski", password: "1234" });
   }, []);
 
   const handleChange = (event) => {
